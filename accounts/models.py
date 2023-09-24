44a13204496ef6180129125313e4563bcc87439f
Your Name <iraqiProfessionalDesigner@gmail.com>
2023-09-24 16:25:15 +0700

    finished

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,13 @@
-from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_token(sender, **kwargs):
+    if kwargs['created']:
+        user = kwargs['instance']
+        Token.objects.get_or_create(user=user)
